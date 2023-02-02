dcc7bf594df589592c3488457ce442580543b2ba
thimagu2023 <124210594+thimagu2023@users.noreply.github.com>
2023-02-02 23:30:07 +0000

    Update build-dev-environment.groovy

--- pipelines/build-dev-environment.groovy
+++ pipelines/build-dev-environment.groovy
@@ -16,6 +16,21 @@ pipeline {
     }
   
     stages {
+         stage('Check parameters') {
+            steps {     
+              script {
+                def port = Integer.parseInt(System.getenv("MYSQL_PORT"))
+                if !(port >= 0 && port <= 65535) {
+                    throw new IllegalArgumentException("Invalid port number: " + name + ". Port must be between 0 and 65535.")
+                }
+                // Empty root password forbidden
+                String password = System.getenv("MYSQL_PASSWORD")
+                if (password.trim().isEmpty()) {
+                  throw new IllegalArgumentException("The MYSQL_PASSWORD parameter cannot be empty.")
+                }
+              }
+            }
+        }
         stage('Checkout GIT repository') {
             steps {     
               script {
